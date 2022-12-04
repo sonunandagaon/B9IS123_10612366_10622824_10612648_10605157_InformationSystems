6287245a85e5a59317f06dfed6a127c7421c759b
Atul721 <atul7721@gmail.com>
2022-12-04 18:47:22 +0000

    Committing the Product Service.

---
+++ MusicEquipmentStore/MusicEquipmentStore/Services/ProductService.cs
@@ -0,0 +1,27 @@
+﻿using MusicEquipmentStore.Context;
+using MusicEquipmentStore.Models;
+
+namespace MusicEquipmentStore.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly DatabaseContext _context;
+
+        public ProductService(DatabaseContext context)
+        {
+            _context = context;
+        }
+        public Products GetProductsDetails()
+        {
+            return _context.Products.SingleOrDefault();
+        }
+
+
+        public Products SaveProductDetails(Products products)
+        {
+            _context.Products.Add(products);
+            _context.SaveChanges();
+            return products;
+        }
+    }
+}
