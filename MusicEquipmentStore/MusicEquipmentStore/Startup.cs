be2ad4a530d6703ed3749c2181fcf6628db1f6d8
Snehal Rajendrakumar Baviskar <10612648@mydbs.ie>
2022-12-04 18:42:15 +0000

    Startup class added with constructor

---
+++ MusicEquipmentStore/MusicEquipmentStore/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace MusicEquipmentStore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+    }
+}
