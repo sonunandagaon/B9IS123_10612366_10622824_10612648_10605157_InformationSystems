86e587495efd4bba7577cb9d28598f2bab20ac0d
sonu10622824 <sonavva.nandagaon@gmail.com>
2022-12-04 16:06:33 +0000

    database context added

---
+++ MusicEquipmentStore/MusicEquipmentStore/Context/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicEquipmentStore.Models;
+
+namespace MusicEquipmentStore.Context
+{
+    public class DatabaseContext :DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) :base (options)
+        {
+
+        }
+        public DbSet<Products> Products { get; set; }
+    }
+}
