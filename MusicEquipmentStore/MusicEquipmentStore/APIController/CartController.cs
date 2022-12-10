ec6cafd0d20548333474fb7b986a4059bbd44681
Snehal Rajendrakumar Baviskar <10612648@mydbs.ie>
2022-12-10 22:22:44 +0000

    Cart Controller added along with constructor

---
+++ MusicEquipmentStore/MusicEquipmentStore/APIController/CartController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicEquipmentStore.Services;
+using System.Data.SqlClient;
+
+namespace MusicEquipmentStore.APIController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+
+        private IProductService _productService;
+
+        public CartController(IProductService productService)
+        {
+            _productService = productService;
+
+        }
+
+    }
+}
