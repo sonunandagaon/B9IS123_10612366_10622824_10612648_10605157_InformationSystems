67167cfcf62731c178181f0e51423d7e37472742
Atul721 <atul7721@gmail.com>
2022-12-10 22:32:53 +0000

    cart model added

---
+++ MusicEquipmentStore/MusicEquipmentStore/Models/Cart.cs
@@ -0,0 +1,13 @@
+﻿namespace MusicEquipmentStore.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductPrice { get; set; }
+        public int ProductId { get; set; }
+        public string ProductQuantity { get; set; }
+        public string UserName { get; set; }
+        public string UserAddress { get; set; }
+    }
+}
