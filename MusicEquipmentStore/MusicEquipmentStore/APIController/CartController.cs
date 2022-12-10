67778db8db2f73bd0f15ecbe26e42f2850c6a0af
Snehal Rajendrakumar Baviskar <10612648@mydbs.ie>
2022-12-10 22:24:20 +0000

    Database Connection string added

--- MusicEquipmentStore/MusicEquipmentStore/APIController/CartController.cs
+++ MusicEquipmentStore/MusicEquipmentStore/APIController/CartController.cs
@@ -12,9 +12,12 @@ namespace MusicEquipmentStore.APIController
 
         private IProductService _productService;
 
+        SqlConnection myConnection = new SqlConnection();
+
         public CartController(IProductService productService)
         {
             _productService = productService;
+            myConnection.ConnectionString = @"Server=DESKTOP-UDLAN01\SQLEXPRESS;Database=Products;Trusted_Connection=True;TrustServerCertificate=true;";
 
         }
 
