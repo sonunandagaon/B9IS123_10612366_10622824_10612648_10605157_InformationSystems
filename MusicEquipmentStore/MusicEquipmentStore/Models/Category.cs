350f035b698207fbf7631c1c7d9cc009a99d89f0
sonu10622824 <sonavva.nandagaon@gmail.com>
2022-12-13 14:54:50 +0000

    required models added

---
+++ MusicEquipmentStore/MusicEquipmentStore/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+using System.Runtime.InteropServices;
+
+namespace MusicEquipmentStore.Models
+{
+    public class Category
+    {
+        
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+
+
+    }
+}
---
+++ MusicEquipmentStore/MusicEquipmentStore/Models/ViewModel/SmallCartViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicEquipmentStore.Models.ViewModel
+{
+    public class SmallCartViewModel
+    {
+        public int NumberOfItems { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
