fcb4e49e41d8238ec7456399a6d1d36902898d3a
snavin <mightymechnavi@gmail.com>
2022-12-16 02:04:54 +0000

    Sweet Alert Modal popup for the View Checkout of the products

--- MusicEquipmentStore/MusicEquipmentStore/Controllers/ProductsController.cs
+++ MusicEquipmentStore/MusicEquipmentStore/Controllers/ProductsController.cs
@@ -15,29 +15,6 @@ namespace MusicEquipmentStore.Controllers
             _context = context;
         }
 
-        //public async Task<IActionResult> Products(string categorySlug = "", int p = 1)
-        //{
-        //    int pageSize = 3;
-        //    ViewBag.PageNumber = p;
-        //    ViewBag.PageRange = pageSize;
-        //    ViewBag.CategorySlug = categorySlug;
-
-        //    if (categorySlug == "")
-        //    {
-        //        ViewBag.TotalPages = (int)Math.Ceiling((decimal)_context.Products.Count() / pageSize);
-
-        //        return View(await _context.Products.OrderByDescending(p => p.Id).Skip((p - 1) * pageSize).Take(pageSize).ToListAsync());
-        //    }
-
-        //    Category category = await _context.Categories.Where(c => c.Slug == categorySlug).FirstOrDefaultAsync();
-        //    if (category == null) return RedirectToAction("Index");
-
-        //    var productsByCategory = _context.Products.Where(p => p.CategoryId == category.Id);
-        //    ViewBag.TotalPages = (int)Math.Ceiling((decimal)productsByCategory.Count() / pageSize);
-
-        //    return View(await productsByCategory.OrderByDescending(p => p.Id).Skip((p - 1) * pageSize).Take(pageSize).ToListAsync());
-        //}
-
         public async Task<IActionResult> ViewProduct(int id)
         {
             ProductViewModel productViewModel = new ProductViewModel();
